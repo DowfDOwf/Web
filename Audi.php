4e171dec03a09395d956e78b14cde85fdc810c22
DowfDOwf <66231763+DowfDOwf@users.noreply.github.com>
2020-06-01 09:20:22 +0700

    Create Audi.php

---
+++ Audi.php
@@ -0,0 +1,46 @@
+# Web
+Web_auto
+</body>
+
+</html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <title>Đăng nhập thành viên</title>
+</head>
+
+<body>
+    <div class="header">
+        Sơn-Đức Auto
+    </div>
+    <div class="topnav">
+        <a href="">Thông tin các loại xe</a>
+        <a href="">Đánh giá người dùng</a>
+        <a href="">Liên hệ</a>
+    </div>
+    <div class="column side">
+        <h4>Hãng xe</h4>
+        <ul>
+            <li class="li"><a href="">Vinfast</a></li>
+            <li class="li"><a href="">Mercedes-Benz</a> </li>
+            <li class="li"><a href="">BMW</a></li>
+            <li class="li"><a href="">Audi</a></li>
+        </ul>
+    </div>
+    <div class="column middle">
+        <p>Hiển thị sản phẩm ở dây</p>
+    </div>
+    <div class="column side">
+        zxc
+    </div>
+    <div class="footer">
+
+    </div>
+    <script src="../web_oto/javascript/js.js"></script>
+</body>
+
+</html>
